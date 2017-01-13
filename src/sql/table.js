ad3dcb2eac72e7179d8e029aeb4d6435ed5b7034
ergl <ergl@users.noreply.github.com>
2017-01-13 18:22:22 +0100

    Support scan range operation
    Only works on primary keys for now

--- src/sql/table.js
+++ src/sql/table.js
@@ -45,8 +45,57 @@ function rawInsert(remote, table, mapping) {
     })
 }
 
+// TODO: Maybe change range from a simple array into something more comples
+function scan(remote, table, range, {in_tx} = {in_tx: true}) {
+    const runnable = tx => rawScan(tx, table, range)
+
+    if (in_tx && remote.startTransaction !== undefined) {
+        return kv.runT(remote, runnable)
+    }
+
+    return runnable(remote)
+}
+
+// For every key in range:
+// - Read that key encoding
+// -- For every field in schema (without pk field)
+// -- Read encoding(key+field)
+// TODO: Compare range against keyrange and throw on key outside of it?
+function rawScan(remote, table, range) {
+    const f_schema = tableMetadata.getSchema(remote, table)
+    return f_schema.then(schema => {
+        const keys = range.map(k => keyEncoding.encodePrimary(table, k))
+
+        const f_pk_field = tableMetadata.getPKField(remote, table)
+        const f_only_fields = f_pk_field.then(pk_field => schema.filter(f => f !== pk_field))
+
+        const f_results = keys.map((key, idx) => {
+            return f_pk_field.then(pk_field => {
+                return f_only_fields.then(only_fields => {
+                    const field_keys = only_fields.map(f => keyEncoding.encodeField(table, range[idx], f))
+                    return scanFields(remote, field_keys.concat(key), only_fields.concat(pk_field))
+                })
+            })
+        })
+
+        return Promise.all(f_results).catch(_ => {
+            // FIXME: Return something else, this will swallow all other values
+            return []
+        })
+    })
+}
+
+function scanFields(remote, field_keys, fields) {
+    return kv.get(remote, field_keys).then(values => {
+        return values.reduce((acc, val, idx) => {
+            const field_name = fields[idx]
+            return Object.assign(acc, {[field_name]: val})
+        }, {})
+    })
+}
 
 module.exports = {
     create,
     insertInto,
+    scan
 }
