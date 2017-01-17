f1be2bf43633d0ea66805b046ab2b40d9238557f
ergl <ergl@users.noreply.github.com>
2017-01-17 16:31:30 +0100

    Add documentation

--- src/sql/table.js
+++ src/sql/table.js
@@ -1,23 +1,23 @@
+const utils = require('../utils')
+
 const kv = require('../db/kv')
 const pks = require('./meta/pks')
 const _schema = require('./meta/schema')
 const keyEncoding = require('../db/keyEncoding')
 const tableMetadata = require('./tableMetadata')
 
-// FIXME: Right now we pick the first field as primary key,
-// let the user pick?
+// TODO: Support user-defined primary keys
 function create(remote, name, schema) {
+    // Pick the head of the schema as an autoincremented primary key
     return tableMetadata.createMeta(remote, name, schema[0], schema)
 }
 
-// TODO: Take fks and indices into account
-// Given the table name, the primary key column name,
-// and a mapping of field names to values
-// 1. Check schema is correct. If it's not, throw
-// 2. Get new pk value by reading the meta keyrange (incrAndGet)
-// 3. Inside a transaction:
-// 3/1. encode(pk) -> pk_value
-// 3/2. for (k, v) in schema: encode(k) -> v
+// See insertInto_Unsafe for details.
+//
+// This function will start a new transaction by default. However,
+// given that the current antidote API doesn't allow nested transactions, this function
+// must be called with `{in_tx: true}` if used inside another transaction.
+//
 function insertInto_T(remote, name, mapping, {in_tx} = {in_tx: false}) {
     const runnable = tx => insertInto_Unsafe(tx, name, mapping)
 
@@ -28,13 +28,37 @@ function insertInto_T(remote, name, mapping, {in_tx} = {in_tx: false}) {
     return kv.runT(remote, runnable, {ignore_ct: false})
 }
 
+// Given a table name, and a map of field names to values,
+// insert those values into the table.
+//
+// If the primary key is in autoincrement mode, then the given map
+// must not contain the primary key field or a value for it.
+//
+// The given field map must be complete, as there is no support for
+// nullable fields right now. Will fail if the schema is not complete.
+// (Sans the pk field restriction pointed above).
+//
+// This function is unsafe. It MUST be ran inside a transaction.
+//
+// TODO: Take fks and indices into account
+// TODO: Allow null values into the database by omitting fields
+// TODO: Support non-numeric primary key values
 function insertInto_Unsafe(remote, table, mapping) {
+    // 1 - Check schema is correct. If it's not, throw
+    // 2 - Get new pk value by reading the meta keyrange (incrAndGet)
+    // 3 - Inside a transaction:
+    // 3.1 - encode(pk) -> pk_value
+    // 3.2 - for (k, v) in schema: encode(k) -> v
     const fields = Object.keys(mapping)
     const values = fields.map(f => mapping[f])
 
+    // Only support autoincrement keys, so calls to insert must not contain
+    // the primary key. Hence, we fetch the primary key field name here.
     return pks.getPKField(remote, table).then(pk_field => {
         return fields.concat(pk_field)
     }).then(schema => {
+        // Inserts must specify every field, don't allow nulls by default
+        // FIXME: Easily solvable by inserting a bottom value.
         return _schema.validateSchema(remote, table, schema).then(r => {
             if (!r) throw "Invalid schema"
 
@@ -50,8 +74,12 @@ function insertInto_Unsafe(remote, table, mapping) {
     })
 }
 
-// TODO: Support more complex selects
-// Right now we only support queries against specific primary keys
+// See select_Unsafe for details.
+//
+// This function will start a new transaction by default. However,
+// given that the current antidote API doesn't allow nested transactions, this function
+// must be called with `{in_tx: true}` if used inside another transaction.
+//
 function select_T(remote, table, fields, pk_value, {in_tx} = {in_tx: false}) {
     const run = tx => select_Unsafe(tx, table, fields, pk_value)
 
@@ -62,12 +90,27 @@ function select_T(remote, table, fields, pk_value, {in_tx} = {in_tx: false}) {
     return kv.runT(remote, run)
 }
 
-// Should always be called from inside a transaction
-function select_Unsafe(remote, table, fields, pk_value) {
-    const pk_values = Array.isArray(pk_value) ? pk_value : [pk_value]
+// select_Unsafe(_, t, [f1, f2, ..., fn], pk) will perform
+// SELECT f1, f2, ..., fn FROM t where {pk_field} = pk
+//
+// Only supports predicates against the primary key field, and restricted
+// to the form `id = x` or `id = a AND id = b (...) AND id = z`. To select
+// more than one key, pass a key list as the last parameter:
+// `select_Unsafe(_, _, _, [k1, k2, ..., kn])`.
+//
+// Supports for wildard select by calling `select_Unsafe(_, _, '*', _)`
+//
+// Will fail if any of the given fields is not part of the table schema.
+//
+// This function is unsafe. It MUST be ran inside a transaction.
+//
+// TODO: Support complex predicates
+function select_Unsafe(remote, table, field, pk_value) {
+    const pk_values = utils.arreturn(pk_value)
+    const fields = utils.arreturn(field)
+
     const perform_scan = lookup_fields => {
-        // We MUST be inside a transaction, so the call to `scan_T` MUST NOT
-        // spawn a new transaction.
+        // We MUST be inside a transaction, so the call to `scan_T` MUST NOT spawn a new transaction.
         return scan_T(remote, table, pk_values, {in_tx: true}).then(res => res.map(row => {
             return Object.keys(row)
                 .filter(k => lookup_fields.includes(k))
@@ -76,7 +119,7 @@ function select_Unsafe(remote, table, fields, pk_value) {
     }
 
     // If we query '*', get the entire schema
-    if (Array.isArray(fields) && fields.length === 1 && fields[0] === '*') {
+    if (fields.length === 1 && fields[0] === '*') {
         return _schema.getSchema(remote, table).then(schema => perform_scan(schema))
     }
 
@@ -87,7 +130,12 @@ function select_Unsafe(remote, table, fields, pk_value) {
 
 }
 
-// TODO: Maybe change range from a simple array into something more comples
+// See scan_Unsafe for details.
+//
+// This function will start a new transaction by default. However,
+// given that the current antidote API doesn't allow nested transactions, this function
+// must be called with `{in_tx: true}` if used inside another transaction.
+//
 function scan_T(remote, table, range, {in_tx} = {in_tx: false}) {
     const runnable = tx => scan_Unsafe(tx, table, range)
 
@@ -98,7 +146,15 @@ function scan_T(remote, table, range, {in_tx} = {in_tx: false}) {
     return kv.runT(remote, runnable)
 }
 
-// TODO: Support index keys
+// Given a table name, and a list of primary keys, will recursively
+// retrieve all the subkeys of each key, returning a list of rows.
+//
+// Will fail if the scan goes out of bounds of max(table.pk_value)
+//
+// This function is unsafe. It MUST be ran inside a transaction.
+//
+// TODO: Support any key with subkeys (pks, index names, index pks)
+// TODO: Change range from array to something more complex
 function scan_Unsafe(remote, table, range) {
     // Assumes keys are numeric
     const f_cutoff = pks.getCurrentKey(remote, table).then(m => {
@@ -133,6 +189,8 @@ function scan_Unsafe(remote, table, range) {
     })
 }
 
+// Given a list of encoded keys, and a matching list of field names,
+// build an object s.t. `{f: get(k)}` for every k in field_keys, f in fields
 function scanFields(remote, field_keys, fields) {
     return kv.get(remote, field_keys).then(values => {
         return values.reduce((acc, val, idx) => {
