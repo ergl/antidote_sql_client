4e99a7cb8529984667f7f1bb38d2c57c71cf63d4
ergl <ergl@users.noreply.github.com>
2017-01-24 16:18:29 +0100

    Implement map over objects

--- src/utils/index.js
+++ src/utils/index.js
@@ -10,7 +10,37 @@ function flatten(arr) {
     }, [])
 }
 
+function mapOKeys(obj, fn) {
+    const old_keys = Object.keys(obj)
+    return old_keys.reduce((acc, curr_key) => {
+        return Object.assign(acc, {
+            [fn(curr_key)]: obj[curr_key]
+        })
+    }, {})
+}
+
+function mapOValues(obj, fn) {
+    return Object.keys(obj).reduce((acc, curr_key) => {
+        return Object.assign(acc, {
+            [curr_key]: fn(obj[curr_key])
+        })
+    }, {})
+}
+
+function mapO(obj, fn) {
+    return Object.keys(obj).reduce((acc, curr_key) => {
+        const [new_k, new_v] = fn(curr_key, obj[curr_key])
+        return Object.assign(acc, {
+            [new_k]: new_v
+        })
+    }, {})
+}
+
 module.exports = {
+    flatten,
     arreturn,
-    flatten
+
+    mapO,
+    mapOKeys,
+    mapOValues
 }
