a46c3262893da2c7b6cc9ffe0f1a4632702e4b05
ergl <ergl@users.noreply.github.com>
2017-02-20 19:36:50 +0100

    Take into account get unwrapping behavior

--- src/sql/meta/pks.js
+++ src/sql/meta/pks.js
@@ -9,8 +9,7 @@ const keyEncoding = require('./../../db/keyEncoding');
 //
 function getPKField(remote, table_name) {
     const meta_key = keyEncoding.table(table_name);
-    return kv.get(remote, meta_key).then(values => {
-        const meta = values[0];
+    return kv.get(remote, meta_key).then(meta => {
         const pk_field = meta.primary_key_field;
         if (pk_field === undefined) {
             throw new Error(`Fatal: ${table_name} doesn't have a primary key`);
@@ -37,15 +36,14 @@ function fetchAddPrimaryKey_T(remote, table_name) {
 
 function getCurrentKey(remote, table_name) {
     const meta_key = keyEncoding.table(table_name);
-    return kv.get(remote, meta_key).then(values => values[0].current_pk_value);
+    return kv.get(remote, meta_key).then(meta => meta.current_pk_value);
 }
 
 // Atomically increment the primary key counter value.
 function incrKey(remote, table_name) {
     const meta_key = keyEncoding.table(table_name);
     return kv.runT(remote, function(tx) {
-        return kv.get(tx, meta_key).then(values => {
-            const meta = values[0];
+        return kv.get(tx, meta_key).then(meta => {
             const pk_value = meta.current_pk_value;
             return kv.put(
                 tx,
