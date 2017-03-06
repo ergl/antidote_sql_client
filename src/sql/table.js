8e357ea492a0de9b42ef2028d2147b55548bfad9
ergl <ergl@users.noreply.github.com>
2017-03-06 17:38:20 +0100

    Implement checkInFKViolation_Unsafe
    Our current foreign key guarantees specify that a value on the parent
    table can only be changed if the current value is not being referenced
    in the child table.
    
    An update operation will fail if trying to update a referenced field.

--- src/sql/table.js
+++ src/sql/table.js
@@ -156,6 +156,50 @@ function checkOutFKViolation_Unsafe(remote, table, mapping) {
     });
 }
 
+// Given a table, and a map of field names to their values (candidate to be updated),
+// check if the new values satisfy foreign key constraints, following that:
+//
+// - A value X in the parent column may only be changed or deleted if X does not exist in the child column.
+//
+// Return true if the condition is met. Foreign keys are represented as regular fields,
+// plus some metadata attached to the table. This means that every insert and update has
+// to check in the parent table, and updates to the parent table will have to check
+// referencing tables. In contrast, reads of foreign keys incur no extra cost.
+//
+// This function is unsafe. It MUST be ran inside a transaction.
+//
+// TODO: Revisit assumptions
+// Assumptions: Given that FK can only be placed on primary keys,
+// we don't have to check that the new value is repeated, as that is asserted by
+// the current `update` behaviour. We only need to check that the old value is not being used
+function checkInFKViolation_Unsafe(remote, table, mapping) {
+    const f_inFKs = fks.getInFKs(remote, table);
+
+    // TODO: Valid for now, change if primary keys are user defined, and / or when fks
+    // may point to arbitrary fields
+    //
+    // Foreign keys may be only created against primary keys, not arbitrary fields
+    // And given that primary keys are only autoincremented, and the database is append-only
+    // We can check if a specific row exists by checking it its less or equal to the keyrange
+    // The actual logic for the cutoff is implemented inside select
+    return f_inFKs.then(inFKs => {
+        const validChecks = inFKs.map(({ reference_table, field_name }) => {
+            // The predicate will be "WHERE field_name = OLD_FK_VALUE"
+            // This should return 0 rows to be value
+            const predicate = { [field_name]: mapping[field_name] };
+            const f_select = select(remote, reference_table, field_name, predicate);
+
+            // In this case, a cutoff error should not happen,
+            // as we're selecting a non-pk value
+            return f_select.then(rows => {
+                return rows.length === 0;
+            });
+        });
+
+        return Promise.all(validChecks).then(allChecks => allChecks.every(Boolean));
+    });
+}
+
 // See select_Unsafe for details.
 //
 // This function will start a new transaction by default, unless called from inside
